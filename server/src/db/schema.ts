00b0fd0b4782df020f39a46b889f808c0c986968
vinay-04 <vinayrajan04@gmail.com>
2024-12-26 17:53:10 +0530

    add dockerfile

--- server/src/db/schema.ts
+++ server/src/db/schema.ts
@@ -1,10 +1,1 @@
 import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
-
-export const users = pgTable({
-  name: "users",
-  columns: {
-    id: integer({ primaryKey: true }),
-    username: varchar({ unique: true }),
-    password: varchar(),
-  },
-});
