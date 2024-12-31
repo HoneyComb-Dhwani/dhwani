bdde72618503060bbc2c99947ee9502ce24a7840
Inigojeevan <in1442@srmist.edu.in>
2024-12-31 18:20:59 +0530

    feat: Added consultation page

---
+++ client/app/dashboard/admin/consultation/new/page.tsx
@@ -0,0 +1,11 @@
+import Layout from "@/components/dash/Layout";
+import AddConsultation from "@/components/dash/patients/CreateConsultations";
+
+const NewConsultationPage: React.FC = () => {
+  return (
+    <Layout>
+      <AddConsultation />
+    </Layout>
+  );
+}
+export default NewConsultationPage;
