22167cf2a294c6f356cb66a1df47012e31e2b0ee
Inigojeevan <in1442@srmist.edu.in>
2025-01-01 13:37:23 +0530

    feat: Added form to register therapist, reused page to list therapist

---
+++ apps/web/app/dashboard/supervisors/therapists/page.tsx
@@ -0,0 +1,12 @@
+import Layout from '@/components/dash/Layout';
+import TherapistsList from '@/components/dash/therapists/TherapistsList';
+
+const ListTherapistsPage = () => {
+  return (
+    <Layout>
+      <TherapistsList />
+    </Layout>
+  );
+};
+
+export default ListTherapistsPage;
